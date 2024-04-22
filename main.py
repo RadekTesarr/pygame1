09929d3d3aecf97358c27e39c982d9ccb0098913
Radek <radek.tesar135@gmail.com>
2024-04-22 21:31:24 +0200

    18

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import pygame as pg
 
+# Init
+pg.init()
+
+# Window
+width = 600
+height = 300
+screen = pg.display.set_mode((width, height))
+
 # Colors
 black = (0, 0, 0)
 white = (255, 255, 255)
@@ -7,42 +15,28 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 
-# Initialization
-pg.init()
+# System fonts
+# fonts = pg.font.get_fonts()
+# for i in fonts:
+#     print(i)      freesans
 
-# Window
-width = 1000
-height = 600
-screen = pg.display.set_mode((width, height))
-pg.display.set_caption("Game")
+# Font set
+sys_font = pg.font.SysFont("arial", 32)
 
-# Background color
-screen.fill(black)
-
-# Shape
-# pg.draw.rect(screen, red, (100, 100, 120, 120))
-
-# Images
-hp_image = pg.image.load("img/icon.png")
-hp_rect = hp_image.get_rect()
-hp_rect.top = 300
-hp_rect.left = 200
-
-coin_img = pg.image.load("img/coin.png")
-coin_rect = coin_img.get_rect()
-coin_rect.center = (width//2, height//2)
+# Font & text
+sys_text = sys_font.render("Harry Potter", True, white, red)
+sys_text_rect = sys_text.get_rect()
+sys_text_rect.center = (width//2, height//2)
 
 # Mainloop
 lets_continue = True
-
 while lets_continue:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             lets_continue = False
 
-    # Bliting
-    screen.blit(hp_image, hp_rect)
-    screen.blit(coin_img, coin_rect)
+    # Text blit
+    screen.blit(sys_text, sys_text_rect)
 
     # Screen update
     pg.display.update()
